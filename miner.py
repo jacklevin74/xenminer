9814eb901033ab10ef4902166b6baeec07eb07ce
Jack Levin <jack@nventify.com>
2023-09-21 13:09:59 -0700

    add merkle pow

--- miner.py
+++ miner.py
@@ -12,6 +12,24 @@ difficulty = 1
 memory_cost = 1500 
 cores = 1
 account = "0x0A6969ffF003B760c97005e03ff5a9741126167A"
+last_block_url = 'http://xenminer.mooo.com:4445/getblocks/lastblock'
+
+def hash_value(value):
+    return hashlib.sha256(value.encode()).hexdigest()
+
+def build_merkle_tree(elements, merkle_tree={}):
+    if len(elements) == 1:
+        return elements[0], merkle_tree
+
+    new_elements = []
+    for i in range(0, len(elements), 2):
+        left = elements[i]
+        right = elements[i + 1] if i + 1 < len(elements) else left
+        combined = left + right
+        new_hash = hash_value(combined)
+        merkle_tree[new_hash] = {'left': left, 'right': right}
+        new_elements.append(new_hash)
+    return build_merkle_tree(new_elements, merkle_tree)
 
 from datetime import datetime
 def is_within_five_minutes_of_hour():
@@ -80,6 +98,54 @@ def generate_random_sha256(max_length=128):
 from tqdm import tqdm
 import time
 
+def submit_pow(account_address, key, hash_to_verify):
+    # Download last block record
+    url = last_block_url
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        records = json.loads(response.text)
+        verified_hashes = []
+
+        for record in records:
+            block_id = record.get('block_id')
+            record_hash_to_verify = record.get('hash_to_verify')
+            record_key = record.get('key')
+            account = record.get('account')
+
+            # Verify each record using Argon2
+            if argon2.verify(record_key, record_hash_to_verify):
+                verified_hashes.append(hash_value(str(block_id) + record_hash_to_verify + record_key + account))
+
+        # If we have any verified hashes, build the Merkle root
+        if verified_hashes:
+            merkle_root, _ = build_merkle_tree(verified_hashes)
+
+            # Calculate block ID for output (using the last record for reference)
+            output_block_id = int(block_id / 100)
+
+            # Prepare payload for PoW
+            payload = {
+                'account_address': account_address,
+                'block_id': output_block_id,
+                'merkle_root': merkle_root,
+                'key': key,
+                'hash_to_verify': hash_to_verify
+            }
+
+            # Send POST request
+            pow_response = requests.post('http://xenminer.mooo.com:4446/send_pow', json=payload)
+
+            if pow_response.status_code == 200:
+                print(f"Proof of Work successful: {pow_response.json()}")
+            else:
+                print(f"Proof of Work failed: {pow_response.json()}")
+
+            print(f"Block ID: {output_block_id}, Merkle Root: {merkle_root}")
+
+    else:
+        print("Failed to fetch the last block.")
+
 # ANSI escape codes
 RED = "\033[31m"
 GREEN = "\033[32m"
@@ -125,7 +191,7 @@ def mine_block(stored_targets, prev_hash):
                         break
                     else:
                         found_valid_hash = False
-                        return 2
+                        break
 
 
             pbar.update(1)
@@ -160,6 +226,10 @@ def mine_block(stored_targets, prev_hash):
 
         # Print the HTTP status code
         print("HTTP Status Code:", response.status_code)
+
+        if target == "XEN11" and found_valid_hash and response.status_code == 200:
+            #submit proof of work validation of last sealed block
+            submit_pow(account, random_data + prev_hash, hashed_data)
 
         if response.status_code != 500:  # If status code is not 500, break the loop
             print("Server Response:", response.json())
