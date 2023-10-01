adf288053bb2f1bc5ec61293492052bf90e35373
WoodySoil <shuankaiwu@gmail.com>
2023-10-02 02:19:41 +0800

    add a miner.sh to start gpuminer in batch

---
+++ miner.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+dev_fee_on=false
+runs=1
+while getopts "c:do" opt; do
+    case "$opt" in
+    c) runs="$OPTARG" ;;
+    d) dev_fee_on=true ;;
+    o) opencl=true ;;
+    esac
+done
+
+command="python3 miner.py"
+if $dev_fee_on; then
+    command+=" --dev-fee-on"
+fi
+
+screen -S "miner" -dm bash -c "$command"
+
+screen -S "gpuminer" -dm
+for ((i = 0; i < $runs; i++)); do
+    command="./xengpuminer -d $i"
+    if $opencl; then
+        command+=" -m opencl"
+    fi
+    screen -S "gpuminer" -X screen bash -c "$command"
+done
+
+echo "Successfully started $runs processes."
