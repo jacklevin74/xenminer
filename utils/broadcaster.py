5d785d27600c9d002e2d15d3a24727ed1ede7329
root <root@server.bffy.io>
2023-10-31 07:00:43 +0000

    broadcaster refactor

--- utils/broadcaster.py
+++ utils/broadcaster.py
@@ -1,10 +1,13 @@
 import asyncio
 import time
+import json
 import websockets
 import zlib
+import redis
 import sqlite3
 import hashlib
 from datetime import datetime
+from flask import Flask, render_template
 
 DB_PATH = 'blocks.db'
 MAX_ERRORS = 100  # Maximum allowed errors before blocking a client
@@ -16,6 +19,10 @@ message_queues = {}     # Mapping client IP to its asyncio queue
 new_data_event = {}     # Event to notify new data availability
 last_processed_id = 0   # Initialize the last processed ID
 error_counters = {}     # Error counters for each client
+received_data_per_client = {}  # Data structure to store received JSON data
+
+# Initialize Redis connection
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 async def print_error_counters_periodically():
     while True:
@@ -36,7 +43,7 @@ def init_db():
     last_processed_id = row[0] if row and row[0] else 0
     conn.close()
 
-# Handle WebSocket messages
+
 async def server_handler(websocket, path):
     client_ip = websocket.remote_address[0]
     hello_count[client_ip] = 0
@@ -44,25 +51,40 @@ async def server_handler(websocket, path):
     new_data_event[client_ip] = asyncio.Event()
     connected_clients[client_ip] = websocket
 
-    try:
+    async def receive_messages():
         async for message in websocket:
-            if message == "Hello":
-                hello_count[client_ip] += 1
-                await websocket.send("Pong")
-                if hello_count[client_ip] == 5:
-                    print(f"Client {client_ip} is ready for transmission!")
-                    if error_counters.get(client_ip, 0) <= MAX_ERRORS:
-                        asyncio.create_task(send_messages(client_ip))
-                    else:
-                        print(f"Client {client_ip} is blocked due to excessive errors.")
-            else:
-                print(f"Received unknown message from {client_ip}: {message}")
+            try:
+                data = json.loads(message)
+                if isinstance(data, dict) and 'block_id' in data and 'hash' in data and 'time_diff' in data:
+                    # Processing of the message
+                    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+                    data['node'] = client_ip
+                    data['timestamp'] = timestamp
+                    received_data_per_client[client_ip] = data
+                    redis_client.set(client_ip, json.dumps(data))
+                    retrieved_data = redis_client.get(client_ip).decode('utf-8')
+                    print(retrieved_data)
+                else:
+                    print(f"Received unknown format message from {client_ip}: {message}")
+            except json.JSONDecodeError:
+                print(f"Invalid JSON received from {client_ip}: {message}")
+
+    try:
+        if error_counters.get(client_ip, 0) <= MAX_ERRORS:
+            print("Created send message task for: ", client_ip)
+            send_task = send_messages(client_ip, websocket, message_queues[client_ip])
+            receive_task = receive_messages()
+            await asyncio.gather(send_task, receive_task)
+        else:
+            print(f"Client {client_ip} is blocked due to excessive errors.")
+
     except websockets.exceptions.ConnectionClosedOK:
         print(f"Connection closed cleanly with {client_ip}")
     except Exception as e:
         print(f"Error with client {client_ip}: {e}")
     finally:
         clean_up_client(client_ip)
+
 
 def clean_up_client(client_ip):
     """ Function to clean up client data from various structures. """
@@ -75,10 +97,9 @@ def clean_up_client(client_ip):
     if client_ip in new_data_event:
         del new_data_event[client_ip]
 
-# Send messages to clients
-async def send_messages(client_ip):
-    ws = connected_clients[client_ip]
-    mq = message_queues[client_ip]
+async def send_messages(client_ip, ws, mq):
+    if client_ip == "72.130.199.204":
+        print("In send_messages: ", client_ip)
 
     while True:
         try:
@@ -96,6 +117,7 @@ async def send_messages(client_ip):
                 print(f"Exceeded max errors for {client_ip}. Blocking communication.")
                 break
 
+
 async def query_data():
     global last_processed_id
     conn = sqlite3.connect(DB_PATH, check_same_thread=False)
@@ -112,6 +134,8 @@ async def query_data():
             print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')} {hash}")
 
             for ip in connected_clients:
+                if ip == "72.130.199.204":
+                    print("IP IN CONNECTED CLIENTS:", ip)
                 await message_queues[ip].put(whole_row)
 
             last_processed_id = id
@@ -128,7 +152,7 @@ init_db()
 
 async def start_server():
     # Start the WebSocket server
-    async with websockets.serve(server_handler, "0.0.0.0", 6667, max_size=None, ping_interval=None) as server:
+    async with websockets.serve(server_handler, "0.0.0.0", 6668, max_size=None, ping_interval=None) as server:
         print_task = asyncio.create_task(print_error_counters_periodically())
         query_task = asyncio.create_task(query_data())
 
