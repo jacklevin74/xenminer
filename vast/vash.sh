d86d12a563b137d8eab692e5ecbe5b4918b38c76
Jack Levin <jacklevin@jacksmacstudio.lan>
2023-11-05 12:42:16 -1000

    vast config

---
+++ vast/vash.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Update package list and install necessary packages
+
+sudo apt update
+sleep 5
+echo "---------------------------"
+
+sudo apt -y install ocl-icd-opencl-dev
+sleep 5
+echo "---------------------------"
+
+sudo apt -y install nano
+sleep 5
+echo "---------------------------"
+
+sudo apt -y install htop
+sleep 5
+echo "---------------------------"
+
+# sudo apt -y install nvtop
+sudo apt -y install cmake
+sleep 5
+echo "---------------------------"
+
+sudo apt -y install python3-pip
+sleep 5
+echo "---------------------------"
+
+# Clone the repository and build the project
+git clone https://github.com/shanhaicoder/XENGPUMiner.git
+sleep 5
+echo "---------------------------"
+
+cd XENGPUMiner
+sleep 5
+echo "---------------------------"
+
+chmod +x build.sh
+sleep 5
+echo "---------------------------"
+
+./build.sh
+sleep 5
+echo "---------------------------"
+
+# Update the configuration file
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xCbFdb56d42a324cEA11C41D187723a6d5e8609eE/g' config.conf
+sleep 5
+echo "---------------------------"
+
+# Install Python requirements
+sudo pip install -U -r requirements.txt
+sleep 5
+echo "---------------------------"
+
+sudo nohup python3 miner.py --gpu=true > miner.log 2>&1 &
+sleep 5
+echo "---------------------------"
+
+sudo nohup ./xengpuminer > xengpuminer.log 2>&1 &
+sleep 5
+echo "---------------------------"
